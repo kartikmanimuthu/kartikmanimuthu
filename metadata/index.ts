4401959ca827d9c397bde4646f73b8e3e88a8c60
Kartik Manimuthu <kartik@Vishals-MacBook-Pro.local>
2023-08-05 17:43:41 +0530

    feat: portfolio website powered by nextra

---
+++ metadata/index.ts
@@ -0,0 +1,63 @@
+export const workExperience: ReadonlyArray<{ organization: string, role: string, duration: string }> = [
+    {
+        organization: "Scrut Automation",
+        role: "Engineering Manager",
+        duration: "01/2023 - Present",
+    },
+    {
+        organization: "BYJU'S (Think & Learn Group)",
+        role: "Principal Architect",
+        duration: "08/2021 - 01/2023",
+    },
+    {
+        organization: "Finicity, a Mastercard Company",
+        role: "Senior Software Engineer",
+        duration: "03/2021 - 08/2021",
+    },
+    {
+        organization: "Citiustech healthcare technology Pvt Ltd.",
+        role: "Senior Software Engineer",
+        duration: "09/2019 - 03/2021",
+    },
+    {
+        organization: "NeoSoft Technologies (Client Company - Edelweiss Financial Services)",
+        role: "Software Engineer",
+        duration: "04/2018 - 09/2019",
+    },
+    {
+        organization: "Sitel India Pvt Ltd",
+        role: "Programmer Analyst",
+        duration: "11/2017 - 04/2018",
+    },
+    {
+        organization: "Select Jobs Pvt Ltd",
+        role: "Software Engineer",
+        duration: "08/2016 - 11/2017",
+    },
+];
+
+
+export const technicalSkills: ReadonlyArray<string> = [
+    "Technical Leadership",
+    "Cloud Architect",
+    "SAAS",
+    "Cloud Cost Optimization",
+    "Devops",
+    "Multi-Tenant Architecture",
+    "Serverless Architecture",
+    "Event Driven Architecture",
+    "System Design",
+    "Performance Engineering",
+    "Query Optimization",
+    "Microfrontend",
+    "Microservices",
+    "Application Performance Monitoring",
+    "Data Engineering",
+    "Data Lake",
+    "Data Warehouse",
+    "ETL/ELT",
+    "MERN Stack",
+    "T3 Stack",
+    "Amazon Web Services",
+    "Golang",
+];
